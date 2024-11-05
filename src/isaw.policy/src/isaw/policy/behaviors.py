de32de327bb16f146368fa92f9e15b87676e180f
alessandro ceglie <sauzher@gmail.com>
2024-11-05 17:51:42 +0100

    added Behavior to handle image ALT text for News

--- src/isaw.policy/src/isaw/policy/behaviors.py
+++ src/isaw.policy/src/isaw/policy/behaviors.py
@@ -3,6 +3,23 @@ from plone.autoform.interfaces import IFormFieldProvider
 from plone.supermodel import model
 from zope import schema
 from zope.interface import provider
+from plone.app.contenttypes.behaviors.leadimage import ILeadImage
+from plone.app.contenttypes import _
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.dexterity.interfaces import IDexterityContent
+from plone.supermodel import model
+from zope import schema
+from zope.component import adapter
+from zope.interface import implementer
+from zope.interface import provider
+from zope.interface import invariant
+from zope.interface import Invalid
+
+class MissingAltText(Invalid):
+    """Missing alt text Exception
+    """
+
+IMAGE_ALT_FIELD_NAME = 'image_alt'
 
 
 @provider(IFormFieldProvider)
@@ -41,3 +58,32 @@ class IEventISAWBehavior(model.Schema):
         description=u"If an RSVP is required, check this box.",
         required=False,
     )
+
+
+
+@provider(IFormFieldProvider)
+class INewsLeadImageISAWBehavior(ILeadImage):
+
+    alt = schema.TextLine(
+        title=_(u'label_leadimage_alt', default=u'Lead Image Alt Text'),
+        description=_(u'help_leadimage_alt', default=u"Accessibility guidelines require an alt text"),
+        required=False,
+    )
+
+    @invariant
+    def validate_alt_image(data):
+        upload = getattr(data, 'image', None)
+        alt = getattr(data, 'alt', None)
+
+        if upload and not alt:
+            raise MissingAltText(_(u"missing_alt_text",
+                default=u"Alt text is required when uploading an image"))
+
+
+
+@implementer(INewsLeadImageISAWBehavior)
+@adapter(IDexterityContent)
+class ISAWNewsLeadImage(object):
+
+    def __init__(self, context):
+        self.context = context
